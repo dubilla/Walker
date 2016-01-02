20e599bff4f7c1d40fdd16e468b00cf92b94ce3b
Dan Ubilla <dan.ubilla@gmail.com>
2016-01-02 13:36:51 -0500

    Add Tournaments to app

---
+++ db/migrate/20160102183208_create_tournaments.rb
@@ -0,0 +1,9 @@
+class CreateTournaments < ActiveRecord::Migration
+  def change
+    create_table :tournaments do |t|
+      t.string :name
+      t.date :start_date
+      t.timestamps null: false
+    end
+  end
+end
